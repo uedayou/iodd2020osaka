41c2cd383846a3c91d8161e92f8110031198459e
uedayou <yooueda@gmail.com>
2020-03-07 18:18:58 +0900

    gaコード追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingId: `UA-159981023-1`,
       },
     },
     `gatsby-plugin-sitemap`,
