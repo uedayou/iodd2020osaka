fe9321814d08e5ff15bb3fd8d7d9be1e9e5cf2dd
uedayou <yooueda@gmail.com>
2020-03-08 06:07:39 +0900

    SEO関連元に戻す

--- lib/gatsby-theme-minimal-blog/src/components/seo.tsx
+++ lib/gatsby-theme-minimal-blog/src/components/seo.tsx
@@ -32,7 +32,7 @@ const SEO = ({ title, description, pathname, image, children }: Props) => {
   } = site
 
   // 引数のpathname が取得できないときは location.pathname を使う
-  pathname = pathname || location.pathname;
+  // pathname = pathname || location.pathname;
   
   const seo = {
     title: title || defaultTitle,
@@ -41,6 +41,30 @@ const SEO = ({ title, description, pathname, image, children }: Props) => {
     image: `${siteUrl}${image || defaultImage}`,
   }
 
+  return (
+    <Helmet title={title} defaultTitle={defaultTitle} titleTemplate={`%s | ${siteTitle}`}>
+      <html lang={siteLanguage} />
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image} />
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:image" content={seo.image} />
+      <meta property="og:type" content="website" />
+      <meta property="og:image:alt" content={seo.description} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:description" content={seo.description} />
+      <meta name="twitter:image" content={seo.image} />
+      <meta name="twitter:image:alt" content={seo.description} />
+      <meta name="twitter:creator" content={author} />
+      <meta name="gatsby-theme" content="@lekoarts/gatsby-theme-minimal-blog" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      {children}
+    </Helmet>
+  )
+  /*
   return (
     <Helmet title={title} defaultTitle={defaultTitle} titleTemplate={`%s | ${siteTitle}`}>
       <html lang={siteLanguage} />
@@ -66,6 +90,7 @@ const SEO = ({ title, description, pathname, image, children }: Props) => {
       {children}
     </Helmet>
   )
+  */
 }
 
 export default SEO
